42ed2e6b962fc6cd11f5798da3ac1e0d106db64e
Ruud Blom <ruudblom@hotmail.com>
2021-01-15 12:33:12 +0100

    first commit

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+echo '------------------------------------------'
+echo 'Begin of Script'
+echo '------------------------------------------'
+echo \
+echo '------------------------------------------'
+echo 'Start updating and upgrading'
+apt update && apt upgrade -y
+#
+
+#
+echo \
+echo '------------------------------------------'
+echo 'End of Script'
+echo '------------------------------------------'
