6d81a7c3d53f1f8c4eacc1bc1b9c8108b5caf1bf
ruudblomnl <75073757+ruudblomnl@users.noreply.github.com>
2021-01-15 12:47:13 +0100

    Update setup-web.sh

--- setup-web.sh
+++ setup-web.sh
@@ -11,7 +11,7 @@ apt update && apt upgrade -y
 apt install -y apache2 php php-xml php-pdo-mysql php-zip php-gd libapache2-mod-php
 a2enmod rewrite
 service apache2 restart
-
+cp phpinfo.php /var/www/html
 chown -R www-data:www-data 
 #
 echo \
